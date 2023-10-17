62aefedda37772e3342ee2ee5bcd584d02c3b8bb
TaviMatasaru <tavi.matasaru@gmail.com>
2023-10-17 10:40:24 +0300

    Add TEMA_1

---
+++ TEMA_1/Assets/attack-idle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class DistanceChecker : MonoBehaviour
+{
+    public GameObject lionTarget;
+    public GameObject tigerTarget;
+
+    private Animator lionAnimator;
+    private Animator tigerAnimator;
+    private float thresholdDistance = 100.0f;
+
+    void Start()
+    {
+        lionAnimator = lionTarget.GetComponentInChildren<Animator>();
+        tigerAnimator = tigerTarget.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (lionTarget.activeSelf && tigerTarget.activeSelf)
+        {
+            float distance = Vector3.Distance(lionTarget.transform.position, tigerTarget.transform.position);
+
+            if (distance < thresholdDistance)
+            {
+                lionAnimator.SetTrigger("TrAttack");
+                tigerAnimator.SetTrigger("TrAttack");
+            }
+            else
+            {
+                lionAnimator.SetTrigger("TrIdle");
+                tigerAnimator.SetTrigger("TrIdle");
+            }
+        }
+    }
+}
